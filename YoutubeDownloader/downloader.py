a18b5a8fe4285a084a1099f5de09990e038aedf4
Dean Johnson <deanjohnson222@gmail.com>
2015-05-27 17:26:12 -0700

    Try again.

--- YoutubeDownloader/downloader.py
+++ YoutubeDownloader/downloader.py
@@ -188,7 +188,7 @@ def filter_results(song, results):
 def is_valid(song, result):
     title = clean(song['title'].strip(' ').lower()).replace(', ', ',').decode('utf8')
     artist = clean(song['artist'].strip(' ').lower()).replace(', ', ',').decode('utf8')
-    yt_title = clean(result['snippet']['title'].lower()).replace(', ', ',').decode('utf8')
+    yt_title = clean(result['snippet']['title'].lower()).replace(', ', ',')
     query = '{0} {1}'.format(title, artist).decode('utf8')
 
     # Songs will likely never be less than 2:20 that I am searching for.
